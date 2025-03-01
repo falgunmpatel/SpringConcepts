54e29f8cbf0918240cbaa39a7f6d3262dc2d20b4
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 22:20:32 +0530

    [Fagun Patel] uc1 - initialize Spring Boot project with Gradle setup and basic application structure
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

---
+++ src/test/java/org/example/springconcepts/SpringConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
