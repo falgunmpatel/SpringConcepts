21fb6ae2349eb51b9837e12ccde41ca7ad0d569b
Falgun Patel <falgunmpatel9123@gmail.com>
2025-03-01 23:38:35 +0530

    [Falgun Patel] uc3 - add DepartmentBean and EmployeeBean with logging and dependency injection
    Signed-off-by: Falgun Patel <falgunmpatel9123@gmail.com>

--- src/main/java/org/example/springconcepts/SpringConceptsApplication.java
+++ src/main/java/org/example/springconcepts/SpringConceptsApplication.java
@@ -1,6 +1,7 @@
 package org.example.springconcepts;
 
 import org.example.springconcepts.component.DemoBean;
+import org.example.springconcepts.component.EmployeeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,10 +11,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringConceptsApplication {
     public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
-     public static void main(String[] args) {
-        logger.debug ("Welcome to Spring Concept Demo");
-        ApplicationContext context = SpringApplication. run (SpringConceptsApplication.class, args);
-        logger.debug ("Checking Context: {}", context.getBean (DemoBean.class) );
+
+    public static void main(String[] args) {
+        logger.debug("Welcome to Spring Concept Demo");
+        ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+        logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
         logger.debug("\n*** Example Using @Autowire annotation on property ***");
+        EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+        employeeBean.setEid(104);
+        employeeBean.setEname("Spring Framework Guru");
+        employeeBean.showEmployeeDetails();
     }
 }
